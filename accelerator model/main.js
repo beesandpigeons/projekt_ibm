f32269dbe2825e5739a01b6d4b9061d3fce0257d
beesandpigeons <134010841+beesandpigeons@users.noreply.github.com>
2026-01-18 19:04:49 +0000

    a

--- accelerator model/main.js
+++ accelerator model/main.js
@@ -47,8 +47,8 @@ spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
-const loader = new GLTFLoader().setPath('linac.gltf');
-loader.load('scene.gltf', (gltf) => {
+const loader = new GLTFLoader().setPath('accelerator model/');
+loader.load('linac.gltf', (gltf) => {
   console.log('loading model');
   const mesh = gltf.scene;
 
