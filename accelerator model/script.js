162e034a1c3ffd1b2184fcc5baf0b9622b08092c
beesandpigeons <134010841+beesandpigeons@users.noreply.github.com>
2026-01-18 18:07:50 +0000

    a

--- accelerator model/script.js
+++ accelerator model/script.js
@@ -1,75 +1,99 @@
-import './three.min.js';
-import './STLLoader.js';
-import './OrbitControls.js';
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-class STLViewer extends HTMLElement {
-  constructor() {
-    super();
+//Create a Three.JS Scene
+const scene = new THREE.Scene();
+//create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//Keep track of the mouse position, so we can make the eye move
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+//Keep the 3D object on a global variable so we can access it later
+let object;
+
+//OrbitControls allow the camera to move around the scene
+let controls;
+
+//Set which object to render
+let objToRender = 'eye';
+
+//Instantiate a loader for the .gltf file
+const loader = new GLTFLoader();
+
+//Load the file
+loader.load(
+  `./models/${objToRender}/scene.gltf`,
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    object = gltf.scene;
+    scene.add(object);
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
   }
+);
 
-  connectedCallback() {
-    this.connected = true;
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-    const shadowRoot = this.attachShadow({ mode: 'open' });
-    const container = document.createElement('div');
-    container.style.width = '100%';
-    container.style.height = '100%';
+//Add the renderer to the DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
 
-    shadowRoot.appendChild(container);
+//Set how far the camera will be from the 3D model
+camera.position.z = objToRender === "dino" ? 25 : 500;
 
-    if (!this.hasAttribute('model')) {
-      throw new Error('model attribute is required');
-    }
+//Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500) //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
 
-    const model = this.getAttribute('model');
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+scene.add(ambientLight);
 
-    let camera = new THREE.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 1, 1000);
-    let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    container.appendChild(renderer.domElement);
-
-    window.addEventListener('resize', function () {
-      renderer.setSize(container.clientWidth, container.clientHeight);
-      camera.aspect = container.clientWidth / container.clientHeight;
-      camera.updateProjectionMatrix();
-    }, false);
-    let controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.enableZoom = true;
-    let scene = new THREE.Scene();
-    scene.add(new THREE.HemisphereLight(0xffffff, 1.5));
-
-    new THREE.STLLoader().load(model, (geometry) => {
-      let material = new THREE.MeshPhongMaterial({
-        color: 0x6A39FF,
-        specular: 100,
-        shininess: 100,
-      });
-      let mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
-
-      let middle = new THREE.Vector3();
-      geometry.computeBoundingBox();
-      geometry.boundingBox.getCenter(middle);
-      mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(-middle.x, -middle.y, -middle.z));
-      let largestDimension = Math.max(geometry.boundingBox.max.x, geometry.boundingBox.max.y, geometry.boundingBox.max.z)
-      camera.position.z = largestDimension * 1.5;
-
-      controls.autoRotate = true;
-      controls.autoRotateSpeed = 1;
-      let animate = () => {
-        controls.update();
-        renderer.render(scene, camera);
-        if (this.connected) {
-          requestAnimationFrame(animate);
-        }
-      };
-      animate();
-    });
-  }
-
-  disconnectedCallback() {
-    this.connected = false;
-  }
+//This adds controls to the camera, so we can rotate / zoom it with the mouse
+if (objToRender === "dino") {
+  controls = new OrbitControls(camera, renderer.domElement);
 }
 
-customElements.define('stl-viewer', STLViewer);
+//Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  //Here we could add some code to update the scene, adding some automatic movement
+
+  //Make the eye move
+  if (object && objToRender === "eye") {
+    //I've played with the constants here until it looked good 
+    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+  }
+  renderer.render(scene, camera);
+}
+
+//Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+//add mouse position listener, so we can make the eye move
+document.onmousemove = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+}
+
+//Start the 3D rendering
+animate();
