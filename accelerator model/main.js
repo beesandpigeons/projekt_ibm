9cf3a0f99fbae8790b8e1cdf4ba07511162264c1
beesandpigeons <134010841+beesandpigeons@users.noreply.github.com>
2026-01-18 18:24:14 +0000

    a

--- accelerator model/main.js
+++ accelerator model/main.js
@@ -28,7 +28,7 @@ const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-  `linac.gltf`,
+  `${objToRender}/linac.gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
